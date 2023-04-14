6e1ea9c6ca9e334a17d0d980257455eeb7d32a2a
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-04-14 17:33:51 -0600

    Controlando datos del formulario

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,51 +1,65 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Input, Icon, Button } from '@rneui/themed';
+import { useFormik } from "formik";
+import { initialValues } from './RegisterForm.data';
 import { styles } from './RegisterForm.styles';
 
 export function RegisterForm() {
-  return (
-    <View style={styles.content}>
-        <Input 
-            placeholder='Correo electronico' 
-            containerStyle={styles.input}
-            rightIcon={
-                <Icon 
-                    type="material-community"
-                    name="at"
-                    iconStyle={styles.icon}
-                />
-            }
-        />
-        <Input 
-            placeholder='Contraseña'
-            containerStyle={styles.input}
-            secureTextEntry={true}
-            rightIcon={
-                <Icon 
-                    type='material-community'
-                    name="eye-outline"
-                    iconStyle={styles.icon}
-                />
-            }
-        />
-        <Input 
-            placeholder='Repetir contraseña'
-            containerStyle={styles.input}
-            secureTextEntry={true}
-            rightIcon={
-                <Icon 
-                    type='material-community'
-                    name="eye-outline"
-                    iconStyle={styles.icon}
-                />
-            }
-        />
-        <Button 
-            title={"Unirse"} 
-            containerStyle={styles.btnContainer} 
-            buttonStyle={styles.btn}
-        />
-    </View>
-  )
+    const formik = useFormik({
+        initialValues: initialValues(),
+        onSubmit: (formValue) => {
+            console.log("Formulario enviado");
+            console.log(formValue);
+        }
+    });
+
+    return (
+        <View style={styles.content}>
+            <Input 
+                placeholder='Correo electronico' 
+                containerStyle={styles.input}
+                rightIcon={
+                    <Icon 
+                        type="material-community"
+                        name="at"
+                        iconStyle={styles.icon}
+                    />
+                }
+                onChangeText={text => formik.setFieldValue("email", text)}
+            />
+            <Input 
+                placeholder='Contraseña'
+                containerStyle={styles.input}
+                secureTextEntry={true}
+                rightIcon={
+                    <Icon 
+                        type='material-community'
+                        name="eye-outline"
+                        iconStyle={styles.icon}
+                    />
+                }
+                onChangeText={text => formik.setFieldValue("password", text)}
+            />
+            <Input 
+                placeholder='Repetir contraseña'
+                containerStyle={styles.input}
+                secureTextEntry={true}
+                rightIcon={
+                    <Icon 
+                        type='material-community'
+                        name="eye-outline"
+                        iconStyle={styles.icon}
+                    />
+                }
+                onChangeText={text => formik.setFieldValue("repeatPassword", text)}
+            />
+            <Button 
+                title={"Unirse"} 
+                containerStyle={styles.btnContainer} 
+                buttonStyle={styles.btn}
+                onPress={formik.handleSubmit}
+            />
+        </View>
+    )
 }
