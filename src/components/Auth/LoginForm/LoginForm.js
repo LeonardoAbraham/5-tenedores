4242853f0dfc3924e302ef9eea7364ab31054997
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-04-17 15:02:03 -0600

    Mostrando y ocultando contraseña

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { Input, Icon, Button } from "@rneui/themed";
 import { styles } from './LoginForm.styles';
 
 
 export function LoginForm() {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const onShowHidePassword = () => setShowPassword((prevState) => !prevState)
+
     return (
         <View style={styles.content}>
             <Input 
@@ -17,9 +22,14 @@ export function LoginForm() {
             <Input 
                 placeholder='Contraseña' 
                 containerStyle={styles.input} 
-                secureTextEntry={true}
+                secureTextEntry={ showPassword ? false : true }
                 rightIcon={
-                    <Icon type="material-community" name="eye-outline" iconStyle={styles.icon} />
+                    <Icon 
+                        type="material-community" 
+                        name={ showPassword ? "eye-off-outline" : "eye-outline" } 
+                        iconStyle={styles.icon} 
+                        onPress={onShowHidePassword}
+                    />
                 }
             />
             <Button title={"Iniciar sesión"} containerStyle={styles.btnContainer} buttonStyle={styles.btn} />
